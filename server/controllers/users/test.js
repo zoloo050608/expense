63e8bcf5d533f62f0f19151a463c7d06c714cb1b
24LP4086 24LP4086 <24LP4086@G305-CC0243.local>
2024-09-30 12:34:30 +0800

    sql

---
+++ server/controllers/users/test.js
@@ -0,0 +1,15 @@
+import { sql } from "../../database/index.js";
+import { v4 as uuid } from "uuid";
+
+export const testController = async (req, res) => {
+  const { username, email, password } = req.body;
+  const createdat = new Date();
+  const updatedat = new Date();
+
+  await sql`INSERT INTO users(username, email, password, createdat, updatedat) VALUES(${username},${email},${password},${createdat}, ${updatedat})`;
+
+  // const result = await sql`SELECT * FROM users`;
+  // console.log(result);
+
+  res.send("success");
+};
--- server/routers/user.router.js
+++ server/routers/user.router.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { SignUp } from "../controllers/users/sign-up.controller.js";
 import { Login } from "../controllers/users/login.controllor.js";
+import { testController } from "../controllers/users/test.js";
 
 const userRouter = Router();
 
 userRouter.route("/user/signup").post(SignUp);
 userRouter.route("/user/login").post(Login);
-
+userRouter.route("/user/dbtest").post(testController);
+userRouter.route("/user/dbtest").get(testController);
 export default userRouter;
